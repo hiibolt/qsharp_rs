97db1b689b30cd7f7a83968adba17f9cbb89d902
BoltR6 <johnwallacewhite@gmail.com>
2023-08-23 21:41:39 -0500

    Added three more basic examples

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,55 @@ use crate::qubit::*;
 use crate::system::*;
 
 fn main() {
-    let mut system = System::new();
+    fn basic_example () {
+        let mut system = System::new();
 
-    let q = system.allocate();
-    q.measure();
+        let q = system.allocate();
+        q.measure();
 
-    // X Gate
-    q.X();
-    q.measure();
+        // X Gate
+        q.X();
+        q.measure();
 
-    // Y Gate
-    q.Y();
-    q.measure();
+        // Y Gate
+        q.Y();
+        q.measure();
 
-    // Z Gate
-    q.Z();
+        // Z Gate
+        q.Z();
 
-    system.dump();
+        system.dump();
+    }
+    fn exercise_1 () {
+        let mut system = System::new();
+
+        let q = system.allocate();
+
+        q.Y();
+        
+        system.dump();
+    }
+    fn exercise_2 () {
+        let mut system = System::new();
+
+        let q = system.allocate_ket("I");
+
+        q.Z();
+        q.Y();
+        q.X();
+
+        system.dump();
+    }
+    fn exercise_3 () {
+        
+    }
+
+    println!("Basic Example\n--------------------------");
+    basic_example();
+
+    println!("Exercise 1:\n--------------------------");
+    exercise_1();
+
+    println!("Exercise 2:\n--------------------------");
+    exercise_2();
 }
